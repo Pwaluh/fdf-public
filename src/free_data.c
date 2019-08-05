539b235de57ad54384523e324307554eb92a388e
drion julien <drionju@gmail.com>
2019-08-05 18:44:03 +0200

    [debbug] - delete the mlx struc after destroying img (struct needed) [feature] - clean list

--- src/free_data.c
+++ src/free_data.c
@@ -6,11 +6,12 @@
 /*   By: judrion <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 19:26:54 by judrion           #+#    #+#             */
-/*   Updated: 2019/08/04 14:50:33 by judrion          ###   ########.fr       */
+/*   Updated: 2019/08/05 16:54:37 by judrion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+#include "keycode.h"
 #include <stdio.h>
 
 void	free_data(t_mlx *mlx)
@@ -19,5 +20,21 @@ void	free_data(t_mlx *mlx)
 	ft_memdel((void**)&mlx->map->data);
 	ft_memdel((void**)&mlx->map);
 	mlx_destroy_image(mlx->ptr, mlx->img_ptr);
-	ft_memdel((void**)&mlx);
+}
+
+void	memdel_size(t_list **list)
+{
+	t_list			*current;
+	t_list			*next;
+
+	current = *list;
+	while (current)
+	{
+		next = current->next;
+		printf("current : %p\t", current);
+		printf("current->next : %p\t", current->next);
+		printf("current->content : %p\t", current->content);
+		printf("str : %s\n", current->content);
+		current = next;
+	}
 }
